bc3c782f0429cf0c3efe57500c670889038d8c44
Jonathan Gammell <gammell@gmail.com>
2015-12-06 20:48:15 +0000

    Whoops
    git-svn-id: svn+asrl://128.100.201.178/var/lib/svn/misc_code/projects/ompl_tests@7981 d8c0fbf6-623e-0410-b3a3-dba22c474feb

--- scripts/bitstar_journal.sh
+++ scripts/bitstar_journal.sh
@@ -13,53 +13,53 @@ R8_TIME=150
 R16_TIME=300
 
 ###Target tolerance###
-../build/Release_1_58_0/bin/informed_journal_targets -r 2 -s $R2_STEER -p TimeVTarget -v 1.05 1.04 1.03 1.02 1.01 1.009 1.008 1.007 1.006 1.005 1.004 1.003 1.002 1.001 1.0005 1.0001 -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_targets -r 8 -s $R8_STEER -p TimeVTarget -v 1.10 1.09 1.08 1.07 1.06 1.05 1.04 1.03 1.02 1.01 -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_targets -r 16 -s $R16_STEER -p TimeVTarget -v 1.10 1.09 1.08 1.07 1.06 1.05 1.04 1.03 1.02 1.01 -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 2 -s $R2_STEER -p TimeVTarget -v 1.05 1.04 1.03 1.02 1.01 1.009 1.008 1.007 1.006 1.005 1.004 1.003 1.002 1.001 1.0005 1.0001 -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 8 -s $R8_STEER -p TimeVTarget -v 1.10 1.09 1.08 1.07 1.06 1.05 1.04 1.03 1.02 1.01 -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 16 -s $R16_STEER -p TimeVTarget -v 1.10 1.09 1.08 1.07 1.06 1.05 1.04 1.03 1.02 1.01 -e 100 -t $R16_TIME
 
 ###Map size###
-../build/Release_1_58_0/bin/informed_journal_targets -r 2 -s $R2_STEER -p TimeVMapSize -v 2.0 5.0 10.0 20.0 30.0 40.0 50.0 -g 1.01 -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_targets -r 8 -s $R8_STEER -p TimeVMapSize -v 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 -g 1.15 -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_targets -r 16 -s $R16_STEER -p TimeVMapSize -v 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 -g 1.15 -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 2 -s $R2_STEER -p TimeVMapSize -v 2.0 5.0 10.0 20.0 30.0 40.0 50.0 -g 1.01 -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 8 -s $R8_STEER -p TimeVMapSize -v 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 -g 1.15 -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_targets -r 16 -s $R16_STEER -p TimeVMapSize -v 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 -g 1.15 -e 100 -t $R16_TIME
 
 ###Regular worlds###
-../build/Release_1_58_0/bin/informed_journal_regular -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_regular -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_regular -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_regular -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_regular -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_regular -r 16 -s $R16_STEER -e 100 -t $R16_TIME
 
 
 ###Random world###
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 2 -s $R2_STEER -e 100 -t $R2_TIME
 
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 8 -s $R8_STEER -e 100 -t $R8_TIME
 
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
-../build/Release_1_58_0/bin/informed_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
+../build/Release_1_58_0/bin/bitstar_journal_random_single -r 16 -s $R16_STEER -e 100 -t $R16_TIME
 
 sudo /home/jdg/Scripts/fan_auto.sh
