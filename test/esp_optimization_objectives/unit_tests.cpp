4ca46ca0ebc460d7323af18e5eafbef03d6b43ab
Marlin Strub <marlin.strub@gmail.com>
2021-03-05 07:26:04 +0100

    Implement test for obstacle clearance objective

--- test/esp_optimization_objectives/unit_tests.cpp
+++ test/esp_optimization_objectives/unit_tests.cpp
@@ -78,4 +78,143 @@ TEST_CASE("Optimization objectives") {
       }
     }
   }
+  
+  SUBCASE("Reciprocal obstacle clearance") {
+    // Load the test configuration.
+    CHECK(fs::exists(configsDir / "obstacle_clearance.json"));
+    config->load(configsDir / "obstacle_clearance.json");
+    
+    // Prepare the context creation.
+    ContextFactory factory(config);
+    const std::vector<std::string> contextNames
+      {"test2d", "test4d", "test8d", "test16d", "test32d"};
+
+    // Loop over all contexts.
+    for (const auto& name : contextNames) {
+      // Create the context.
+      auto context = factory.create(name);
+
+      // Allocate test states.
+      auto s1 = context->getSpaceInformation()->allocState()->as<RealVectorStateSpace::StateType>();
+      auto s2 = context->getSpaceInformation()->allocState()->as<RealVectorStateSpace::StateType>();
+      for (auto i = 0u; i < context->getDimension(); ++i) {
+        (*s1)[i] = 0.0;
+        (*s2)[i] = 0.0;
+      }
+
+      // Get more convenient access to problem components.
+      const auto objective = context->getObjective();
+      const auto spaceInfo = context->getSpaceInformation();
+      const auto checker   = spaceInfo->getStateValidityChecker();
+
+      // Check trivial costs.
+      CHECK(objective->motionCost(s1, s2).value() == 0.0);
+      CHECK(objective->motionCostHeuristic(s1, s2).value() == 0.0);
+
+      // Check cost parallel to obstacle.
+      /*    s1    s2    The cost should be:
+       *     x----x     integral from 0 to 0.2 of 1 / 0.1 dt.
+       *   +--------+   So 10 * 0.2 = 2
+       *   |        |
+       *   |  obs.  |
+       *   |        |
+       *   +--------+
+       */
+      (*s1)[0u] =  0.1;
+      (*s2)[0u] = -0.1;
+      (*s1)[1u] =  0.3;
+      (*s2)[1u] =  0.3;
+
+      // Make sure the setup is actually what's pictured above.
+      CHECK(spaceInfo->distance(s1, s2) == doctest::Approx(0.2));
+      CHECK(checker->clearance(s1) == doctest::Approx(0.1));
+      CHECK(checker->clearance(s2) == doctest::Approx(0.1));
+
+      // Make sure the cost is as expected.
+      CHECK(objective->motionCost(s1, s2).value() == doctest::Approx(2.0));
+
+      // Make sure the heuristic is as expected.
+      const auto heuristicParallel =
+        std::log(std::pow((0.1 + 0.1 + 0.2), 2.0) / (4 * 0.1 * 0.1));
+      CHECK(objective->motionCostHeuristic(s1, s2).value() == doctest::Approx(heuristicParallel));
+
+      // Make sure the heuristic is admissible
+      CHECK_FALSE(objective->isCostBetterThan(objective->motionCost(s1, s2),
+                                              objective->motionCostHeuristic(s1, s2)));
+
+      // Check cost perpendicular to obstacle.
+      /*                        The cost should be:
+       *   +--------+           integral from 0 to 0.2 of 1 / (t + 0.1) dt.
+       *   |        | s1    s2  So [ln(t + 0.1)|_0^0.2 = ln(0.3) - ln(0.1)
+       *   |  obs.  |  x----x                          = ln(3)
+       *   |        |
+       *   +--------+
+       */
+      (*s1)[0u] = -0.3;
+      (*s2)[0u] = -0.5;
+      (*s1)[1u] =  0.0;
+      (*s2)[1u] =  0.0;
+
+      // Make sure the setup is actually what's pictured above.
+      CHECK(spaceInfo->distance(s1, s2) == doctest::Approx(0.2));
+      CHECK(checker->clearance(s1) == doctest::Approx(0.1));
+      CHECK(checker->clearance(s2) == doctest::Approx(0.3));
+
+      // Make sure the cost is as expected.
+      CHECK(objective->motionCost(s1, s2).value() == doctest::Approx(std::log(3)));
+
+      // Make sure the heuristic is as expected.
+      const auto heuristicPerpendicular = std::log(3);
+      CHECK(objective->motionCostHeuristic(s1, s2).value() == doctest::Approx(heuristicPerpendicular));
+
+      // Make sure the heuristic is admissible
+      CHECK_FALSE(objective->isCostBetterThan(objective->motionCost(s1, s2),
+                                              objective->motionCostHeuristic(s1, s2)));
+
+      // Check cost at angle to obstacle.
+      /*                        The cost should be:
+       *   +--------+ s1        integral from 0 to sqrt(0.08) of 1 / ((sqrt(2)/2) t + 0.1) dt.
+       *   |        |  x        So [2/sqrt(2) * ln((sqrt(2)/2)t + 0.1)|_0^0.2 
+       *   |  obs.  |   `-_           = 2/sqrt(2) * ln(10 * (sqrt(2)/2 * sqrt(0.08) + 0.1)).
+       *   |        |      `x
+       *   +--------+       s2
+       */
+      (*s1)[0u] = -0.3;
+      (*s2)[0u] = -0.5;
+      (*s1)[1u] =  0.1;
+      (*s2)[1u] = -0.1;
+
+      // Make sure the setup is actually what's pictured above.
+      CHECK(spaceInfo->distance(s1, s2) == doctest::Approx(std::sqrt(0.08)));
+      CHECK(checker->clearance(s1) == doctest::Approx(0.1));
+      CHECK(checker->clearance(s2) == doctest::Approx(0.3));
+
+      // Make sure the cost is as expected.
+      CHECK(objective->motionCost(s1, s2).value() ==
+            doctest::Approx(2.0/std::sqrt(2.0) *
+                            std::log(10.0 * (std::sqrt(2) /
+                                             2.0 * std::sqrt(0.08) + 0.1))));
+
+      // // Make sure the heuristic is as expected.
+      const auto heuristicAngle = std::log(std::pow(0.1 + 0.3 + std::sqrt(0.08), 2.0) /
+                                           (4.0 * 0.1 * 0.3));
+      CHECK(objective->motionCostHeuristic(s1, s2).value() == doctest::Approx(heuristicAngle));
+
+      // Make sure the heuristic is admissible
+      CHECK_FALSE(objective->isCostBetterThan(objective->motionCost(s1, s2),
+                                              objective->motionCostHeuristic(s1, s2)));
+
+      // Check the admissibility of the heuristic for 1000 random edges.
+      const auto sampler = spaceInfo->allocStateSampler();
+      sampler->setLocalSeed(42u);  // The tests should never fail/succeed randomly.
+      for (auto i = 0u; i < 1000u; ++i) {
+        sampler->sampleUniform(s1);
+        sampler->sampleUniform(s2);
+        // Make sure the heuristic is admissible
+        CHECK_FALSE(objective->isCostBetterThan(objective->motionCost(s1, s2),
+                                                objective->motionCostHeuristic(s1, s2)));
+      }
+      
+    }
+  }
 }
